cc565c497b0476d31291d67e5381ed75ace5e293
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2015-09-24 12:35:25 +0200

    Set add click-brn-libs/lib to LD_LIBARY_PATH

--- install_ns2.sh
+++ install_ns2.sh
@@ -204,7 +204,10 @@ case "$key" in
 	  XCFLAGS="$XCFLAGS -I$CLICKPATH -I$CLICKPATH/include"
 
 	  CLICKLINKERCONFIG=`(cd $CLICKPATH/ns; make linkerconfig)`
-	  (cd $PREFIX/src/ns-2.34; export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PREFIX/lib/:$CLICKPATH/ns/; CFLAGS="$XCFLAGS" CPPFLAGS="$XCFLAGS" CXXFLAGS="$XCFLAGS" LDFLAGS="$XCFLAGS" LIBS="-L$PREFIX/lib/ -L$CLICKPATH/ns/ -L$BRN_TOOLS_PATH/click-brn-libs/lib $CLICKLINKERCONFIG -lstdc++" ./configure --prefix=$PREFIX --with-click=$CLICKPATH --with-tcl=$PREFIX --with-tclcl=$PREFIX --with-tk=$PREFIX --with-otcl=$PREFIX; make -j $CPUS)
+	  echo "$CLICKLINKERCONFIG"
+	  echo "(cd $PREFIX/src/ns-2.34; export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PREFIX/lib/:$CLICKPATH/ns/:$BRN_TOOLS_PATH/click-brn-libs/lib; CFLAGS=\"$XCFLAGS\" CPPFLAGS=\"$XCFLAGS\" CXXFLAGS=\"$XCFLAGS\" LDFLAGS=\"$XCFLAGS\" LIBS=\"-L$PREFIX/lib/ -L$CLICKPATH/ns/ -L$BRN_TOOLS_PATH/click-brn-libs/lib $CLICKLINKERCONFIG -lstdc++\" ./configure --prefix=$PREFIX --with-click=$CLICKPATH --with-tcl=$PREFIX --with-tclcl=$PREFIX --with-tk=$PREFIX --with-otcl=$PREFIX; make -j $CPUS)"
+
+	  (cd $PREFIX/src/ns-2.34; export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PREFIX/lib/:$CLICKPATH/ns/:$BRN_TOOLS_PATH/click-brn-libs/lib; CFLAGS="$XCFLAGS" CPPFLAGS="$XCFLAGS" CXXFLAGS="$XCFLAGS" LDFLAGS="$XCFLAGS" LIBS="-L$PREFIX/lib/ -L$CLICKPATH/ns/ -L$BRN_TOOLS_PATH/click-brn-libs/lib $CLICKLINKERCONFIG -lstdc++" ./configure --prefix=$PREFIX --with-click=$CLICKPATH --with-tcl=$PREFIX --with-tclcl=$PREFIX --with-tk=$PREFIX --with-otcl=$PREFIX; make -j $CPUS)
 	  (if [ -f $PREFIX/bin/ns ]; then mv $PREFIX/bin/ns $PREFIX/bin/ns.old; fi)
 	  ln -s $PREFIX/src/ns-2.34/ns $PREFIX/bin/ns
 	fi
